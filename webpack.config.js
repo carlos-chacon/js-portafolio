0f28d4aaf46544c6545d2d0afc59ad1e172e7026
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2021-05-07 10:16:48 -0500

    HTML en Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -17,5 +18,12 @@ module.exports = {
                 loader: 'babel-loader'
             }
         }]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
 };
