fbd5ea54d6be2b5dcf9ceccf37be37991adc071f
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2021-05-07 10:40:41 -0500

    Loaders para CSS y preprocesadores de CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -17,6 +18,13 @@ module.exports = {
             use: {
                 loader: 'babel-loader'
             }
+        }, {
+            test: /\.css|.styl$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'stylus-loader'
+            ]
         }]
     },
     plugins: [
@@ -24,6 +32,7 @@ module.exports = {
             inject: true,
             template: './public/index.html',
             filename: './index.html'
-        })
+        }),
+        new MiniCssExtractPlugin(),
     ]
 };
