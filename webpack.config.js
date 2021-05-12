44d6c436b0d001e30457050539fdcd868e718ea9
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2021-05-12 15:56:25 -0500

    Variables de entorno14

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const DotEnv = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/index.js',
@@ -67,7 +68,8 @@ module.exports = {
                 from: path.resolve(__dirname, "src", "assets/images"),
                 to: "assets/images"
             }]
-        })
+        }),
+        new DotEnv()
     ],
     optimization: {
         minimize: true,
